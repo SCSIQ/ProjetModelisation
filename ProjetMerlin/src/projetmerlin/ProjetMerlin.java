c8ade7b1ab9fd72008b687b538346517d8289c2c
Aurian <durand.aurian@me.com>
2018-11-10 15:59:10 +0100

    Quelques correctifs

--- ProjetMerlin/src/projetmerlin/ProjetMerlin.java
+++ ProjetMerlin/src/projetmerlin/ProjetMerlin.java
@@ -12,12 +12,14 @@ public class ProjetMerlin {
      */
     public static void main(String[] args) {
 
-        int nbSimulations = 1000000; // Entrez ici le nombre de simulations que vous voulez faire
+        // Entrez ici le nombre de simulations que vous voulez faire
+        int nbSimulations = 1;
 
-        Simulation simulation = new Simulation(); //Crée une banque de simulations
+        //Crée une banque de simulations
+        Simulation simulation = new Simulation();
 
         //ici, vous pouvez lancer n simulations avec ou sans IHM (pour cela modifiez l'appel de fonction)
-        System.out.println("\nPour "+nbSimulations+" simulations, E(x)="+simulation.lancerSansIHM(nbSimulations));
+        System.out.println("\nPour "+nbSimulations+" simulations, E(x)="+simulation.lancerAvecIHM(nbSimulations));
 
     }
 
